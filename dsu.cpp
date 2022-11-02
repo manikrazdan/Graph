a293e9d1e7d3b8e0f886a6aceaf0c6ff9c500dd7
manikrazdan <116128219+manikrazdan@users.noreply.github.com>
2022-11-03 04:26:13 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,43 @@
+class dsu {
+private:
+	vector<ll> parent,rank,size;
+public:
+	dsu(ll n) {
+		parent.resize(n);
+		for (ll i=0;i<=n;i++) {
+			parent[i]=i;
+		}
+		rank.resize(n+1,0);
+		size.resize(n+1,1);
+	}
+	ll findParent(ll x) {
+		if (parent[x]==x) return x;
+		return parent[x]=findParent(parent[x]);
+	}
+	void unionbyrank(ll u,ll v) {
+		ll pu=findParent(u),pv=findParent(v);
+		if (pu==pv) return;
+		if (rank[pu]<rank[pv]) {
+			parent[pu]=pv;
+		}
+		else if (rank[pv]<rank[pu]) {
+			parent[pv]=pu;
+		}
+		else {
+			parent[pv]=pu;
+			rank[pu]++;
+		}
+	}
+	void unionbysize(ll u,ll v) {
+		ll pu=findParent(u),ll v=findParent(v);
+		if (pu==pv) return;
+		if (size[pu]<sizep[pv]) {
+			parent[pu]=pv;
+			size[pv]+=size[pu];
+		}
+		else {
+			parent[pv]=pu;
+			size[pu]+=size[pv];
+		}
+	}
+};
